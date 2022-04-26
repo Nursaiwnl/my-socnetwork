9657fcbe83666d6051a764467f6fe68da671ebd2
Boranbay Nurserik <jackpoteslasama@gmail.com>
2022-04-26 22:32:45 +0600

    1step

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://media.radissonhotels.net/image/destination-pages/localattraction/16256-118729-f63243131_3xl.jpg?impolicy=HighCarrousel'></img>
+    </div>
+    <div>
+      ava+descrip
+    </div>
+    <div>
+      Myposts
+      <div>
+        New post
+      </div>
+    </div>
+    <div>
+      <div>
+        1post
+      </div>
+      <div>
+        2post
+      </div>
+    </div>
+  </div>
+} 
+
+export default Profile;
