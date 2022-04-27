fa03a31d776df9689464b6476b2c3ab90f3026a7
Boranbay Nurserik <jackpoteslasama@gmail.com>
2022-04-27 07:45:52 +0600

    v.4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialog from "./components/Dialog/Dialog";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App=()=> {
   return (
+      <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
-    </div>);
+        <div className='app-wrapper content'>
+          <Routes>
+        <Route path='/dialog' element={<Dialog />}/>
+            <Route path='/profile' element={<Profile />}/>
+            <Route path='/news' element={<News />}/>
+            <Route path='/music' element={<Music />}/>
+            <Route path='/settings' element={<Settings />}/>
+          </Routes>
+        </div>
+    </div>
+      </BrowserRouter>);
 }
 
 export default App;
---
+++ src/components/Dialog/Dialog.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Dialog.module.css";
+
+const Dialog = () => {
+    return (<div>
+        dialog
+    </div>)
+} 
+
+export default Dialog;
